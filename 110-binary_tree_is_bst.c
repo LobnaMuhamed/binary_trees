691f0aa8e65299584d89ebde7aa688e5333f5610
MahmoudEasa <mu01011422865@gmail.com>
2023-08-30 16:39:56 +0000

    Fix 110-binary_tree_is_bst.c 11

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -17,6 +17,9 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
+	if (!tree->left && !tree->right)
+		return (1);
+
 	q = (Q_q *)malloc(sizeof(Q_q));
 	if (!q)
 		return (0);
